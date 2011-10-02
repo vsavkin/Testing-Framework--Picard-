d78da2903c9ba6cfe9b19529fb029df55ff3d85c
Victor Savkin <vic.savkin@gmail.com>
2011-10-02 14:39:53 -0400

    add all necessary requires to the entry point file

--- lib/picard.rb
+++ lib/picard.rb
@@ -1,4 +1,12 @@
 require "picard/version"
 
-module Picard
-end
+require_relative 'picard/extensions'
+require_relative 'picard/context'
+require_relative 'picard/error_message_formatter'
+require_relative 'picard/ast_helper'
+require_relative 'picard/assertion_wrapper'
+require_relative 'picard/s_expression_sugar'
+require_relative 'picard/class_ripper'
+require_relative 'picard/preprocessor'
+require_relative 'picard/method_ripper'
+require_relative 'picard/test_unit'
